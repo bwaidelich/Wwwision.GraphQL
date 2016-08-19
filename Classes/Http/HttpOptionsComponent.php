e843eb59747abff771a98173f54dfd63727152d8
Bastian <b.waidelich@wwwision.de>
2016-08-19 10:56:02 +0200

    FEATURE: support OPTIONS request
    Adds a simple HTTP Component that responds to OPTIONS request with
    a corresponding HTTP response if a corresponding endpoint is configured::
    
       HTTP/1.1 200 OK
       Allow: GET, POST
       Content-Length: 0

---
+++ Classes/Http/HttpOptionsComponent.php
@@ -0,0 +1,39 @@
+<?php
+namespace Wwwision\GraphQL\Http;
+
+use TYPO3\Flow\Annotations as Flow;
+use TYPO3\Flow\Http\Component\ComponentChain;
+use TYPO3\Flow\Http\Component\ComponentContext;
+use TYPO3\Flow\Http\Component\ComponentInterface;
+
+/**
+ * A simple HTTP Component that captures OPTIONS requests and reponds with a general "Allow: GET, POST" header if a matching graphQL endpoint is configured
+ */
+class HttpOptionsComponent implements ComponentInterface
+{
+    /**
+     * @Flow\InjectConfiguration(path="endpoints")
+     * @var array
+     */
+    protected $endpoints;
+
+    /**
+     * @param ComponentContext $componentContext
+     * @return void
+     */
+    public function handle(ComponentContext $componentContext)
+    {
+        $httpRequest = $componentContext->getHttpRequest();
+        // no OPTIONS request => skip
+        if ($httpRequest->getMethod() !== 'OPTIONS') {
+            return;
+        }
+        // no matching graphQL endpoint configured => skip
+        if (!isset($this->endpoints[$httpRequest->getRelativePath()])) {
+            return;
+        }
+        $httpResponse = $componentContext->getHttpResponse();
+        $httpResponse->setHeader('Allow', 'GET, POST');
+        $componentContext->setParameter(ComponentChain::class, 'cancel', true);
+    }
+}
